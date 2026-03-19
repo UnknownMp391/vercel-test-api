0675679a85ddbf37e717473f3773604ed5d99b84
UnknownMp <unknownmp@163.com>
2026-03-19 09:39:34 +0800

    feat: 增加 name 字段

--- api/hello.ts
+++ api/hello.ts
@@ -8,7 +8,8 @@ export default async function handler(request: Request): Promise<Response> {
   
   return new Response(
     JSON.stringify({
-      message: `Hello ${name}!`, 
+      message: `Hello ${name}!`,
+      name
     }),
     {
       status: 200,
